2a79a0a6dc09e82a39bd374a13235179ce80dcf7
r_sasaki <ryosuke_sasaki@nuco.co.jp>
2018-10-24 09:59:44 +0900

    class Hand 簡易的に実装

--- blackjack/src/models/Hand.js
+++ blackjack/src/models/Hand.js
@@ -1,5 +1,26 @@
 
 class Hand {
-  constructor() {
+  constructor(cards) {
+    this.cards = cards
+  }
+
+  add (card) {
+    this.cards.append(card)
+  }
+
+  score () {
+    const _sum = (pre, current) => pre + current
+    const points = this.cards.map(c => c.point)
+    return points.reduce(_sum)
+  }
+
+  isBurst () {
+    return score() > 21
+  }
+
+  containsAce () {
+    return this.cards.filter(c => c.rank === 1).length > 0
   }
 }
+
+export { Hand }
